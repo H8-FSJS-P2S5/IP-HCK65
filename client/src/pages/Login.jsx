cfe0dbe65a35eea280a7509f62d787ccb68bfcdd
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-12-11 23:51:49 +0700

    setup for access_token

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import LoginBtn from "../buttons/LoginBtn";
+
+export default function Login() {
+
+    const SPOTIFY_CLIENT_ID = 'd86db55895ca45319957a258fac15a79'
+    const REDIRECT_URI = "http://localhost:5173/callback"
+    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+    const RESPONSE_TYPE = 'code'
+    const SCOPES = [
+        'streaming',
+        'user-read-private',
+        'user-read-email',
+        'user-read-recently-played',
+        'user-read-playback-state',
+        'user-modify-playback-state',
+        'user-library-modify',
+        'user-library-read',
+        'user-follow-modify',
+        'playlist-read-private',
+        'playlist-modify-public',
+        'playlist-modify-private',
+        'user-top-read'
+    ].join(' ');
+
+    let scopes_encoded = SCOPES.replace(' ', '%20');
+
+    return (
+        <>
+            <a href={`${AUTH_ENDPOINT}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes_encoded}&show_dialog=true`}><LoginBtn /></a>
+        </>
+    )
+}
