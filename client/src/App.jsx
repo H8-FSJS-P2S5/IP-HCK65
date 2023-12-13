f06c23a15ed643001c135ef942cce516266bbd25
Gema Kusuma <gemaakbarkusuma@gmail.com>
2023-12-13 21:30:32 +0700

    feat: setup app, main & draw.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router.jsx";
-
+import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import store from './app/store'
+import {Provider} from 'react-redux'
 
 function App() {
     return (
         <>
-            <ToastContainer/>
-            <RouterProvider router={router}/>
+            <Provider store={store}>
+                <ToastContainer/>
+                <RouterProvider router={router}/>
+            </Provider>
         </>
     )
 }
