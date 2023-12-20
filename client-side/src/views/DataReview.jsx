ea8669da2f85b18d7b0aa39fba3ebdf8054e3f8f
TanyaArdhia <tanyaardhiap@gmail.com>
2023-12-20 22:36:05 +0700

    test

--- client-side/src/views/DataReview.jsx
+++ client-side/src/views/DataReview.jsx
@@ -70,7 +70,7 @@ function DataReview() {
                     type="button"
                     className="text-gray-900 bg-white border border-blue-500 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-blue-600 dark:hover:bg-blue-700 dark:hover:border-blue-600 dark:focus:ring-blue-700"
                   >
-                    <Link to={`/movie/review/edit/${id}`}>Edit</Link>
+                    <Link to={`/movie/review/edit/${item.id}`}>Edit</Link>
                   </button>
                   <button
                     type="button"
